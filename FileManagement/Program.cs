14c5d31bedd6131f8a2ed8bc5088ee5b8fbc662b
Una LEstrange <lestrange.una@itsligo.ie>
2022-03-25 09:34:25 +0000

    Add project files.

---
+++ FileManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
